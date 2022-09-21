375f80d6edd8ae9178074693a777ac4bf6243255
Aminahp <aypitt7@gmail.com>
2022-09-20 20:54:25 -0400

    indes.jsx

--- controllers/flights2.js
+++ controllers/flights2.js
@@ -1,26 +1,44 @@
 const express = require('express');
 const router = express.Router()
-const Flight = require('./models/Flight');
+const Flight = require('./models/Flight.js');
 
 
 // Induces -- if routes put in this order it will never fail you!
 
 //index -- view of ALL ITEMS 
-router.get('/', (req,res) => {
-    //in here goes a FLIght.find({}) to find allof your flights, then a 
-    //res.render to show the view associated w/ them (index.jsx view)
-})
+router.get('/', (req, res) => { // Route is technically /flights because in server.js it is put as the base route for this controller
+    // in here goes a Flight.find({}) to find all of your flights and then a res.render to show the view associated with them! (Index.jsx view)
+    Flight.find({}, (error, allFlights) => {
+      res.render('flights/Index', {
+        flights: allFlights
+      })
+    })
+  })
 //new -- form to CREATE a new item 
 
 router.get('/new', (req,res)=>{
     //in here gors res.render to show 'create a new fligth' form(New.jsx view)
+    // const newFlight = new Flight();
+    // // Obtain the default date
+	// const dt = newFlight.departs;
+	// // Format the date for the value attribute of the input
+	// const departsDate = dt.toISOString().slice(0, 16);
+	res.render('flights/New');
+
 })
 //delete -- action to DELETE an item
 //update -- action to UPDATE an items
 //create -- action to CREATE a new item 
 
 router.post('/',(req,res)=>{ 
-    //in here goes Flight.create(), passing req.body to it & res.redirect to your index page 
-})
+    //in here goes Flight.create(), passing req.body to it & res.redirect to your index page
+    Flight.create(req.body, (error, createdFlight) =>{
+        res.redirect('/flights')
+    });
+});
 //edit -- form to UPDATE an item
+
+
 //show -- view of ONE ITEM
+
+module.exports = router;
--- views/flights/new.jsx
+++ views/flights/new.jsx
@@ -2,9 +2,20 @@ const React = require('react')
 
 const New = (props) => {
     return(
-        <div>
+   <div>
             {/*form for creating a new Flight  */}
-        </div>
+    <h1> New Route </h1>
+
+      <form action="/flights" method="POST">
+        <label>Airline:</label>
+        <input type="text" name="airline" /><br/>
+        <label>flightNo:</label>
+        <input type="text" number="number" /><br/>
+        <label>Departs:</label>
+        <input type="datetime-local"></input>
+        <input type="submit" value="Create Flight!" />
+      </form>
+  </div>
     )
 }
 
